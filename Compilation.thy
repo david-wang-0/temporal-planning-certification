950c3553a57b2ad6ba6c8c5da97f4b54c261eb3d
David Wang <72378768+david-wang-0@users.noreply.github.com>
2024-10-30 13:48:53 +0100

    Define automaton abstractly

--- Compilation.thy
+++ Compilation.thy
@@ -83,20 +83,20 @@ subsubsection \<open>Decision-making\<close>
 definition AND_ALL::"(('proposition, 'action) clock, 't) dconstraint list \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint" where
 "AND_ALL xs = fold AND xs (true_const)"
 
-text \<open>Numbering for snap_actions. This is too difficult to work with.\<close>
-definition s::"nat \<Rightarrow> 'snap_action" where
-"s n \<equiv> if (n mod 2 = 0) then (at_start (act (n div 2))) else (at_end (act (n div 2)))"
+text \<open>Numbering for snap_actions. This is hard to work with.\<close>
+definition snap::"nat \<Rightarrow> 'snap_action" where
+"snap n \<equiv> if (n mod 2 = 0) then (at_start (act (n div 2))) else (at_end (act (n div 2)))"
 
-lemma "bij_betw s {n. n < 2 * M} (at_start ` actions \<union> at_end ` actions)"
+lemma "bij_betw snap {n. n < 2 * M} (at_start ` actions \<union> at_end ` actions)"
 proof -
-  have 1: "bij_betw s {n. n < 2 * M \<and> n mod 2 = 0} (at_start ` actions)"
+  have 1: "bij_betw snap {n. n < 2 * M \<and> n mod 2 = 0} (at_start ` actions)"
   proof -
-    have "inj_on s {n. n < 2 * M \<and> n mod 2 = 0}"
+    have "inj_on snap {n. n < 2 * M \<and> n mod 2 = 0}"
     proof (rule inj_onI)
       fix x y
       assume a: "x \<in> {n. n < 2 * M \<and> n mod 2 = 0}" 
              "y \<in> {n. n < 2 * M \<and> n mod 2 = 0}" 
-             "s x = s y" 
+             "snap x = snap y" 
       
       hence 0: "x div 2 \<in> {m. m < M}"
             "y div 2 \<in> {m. m < M}" by auto
@@ -108,7 +108,7 @@ proof -
       
       from a
       have 2: "at_start (act (x div 2)) = at_start (act (y div 2))"  
-        using s_def by auto
+        using snap_def by auto
       
       from 1 2
       have "act (x div 2) = act (y div 2)" 
@@ -122,17 +122,17 @@ proof -
       show "x = y" by (metis div_mod_decomp)
     qed
     moreover
-    have "s ` {n. n < 2 * M \<and> n mod 2 = 0} = (at_start ` actions)"
+    have "snap ` {n. n < 2 * M \<and> n mod 2 = 0} = (at_start ` actions)"
     proof (rule equalityI; rule subsetI)
       fix x
-      assume "x \<in> s ` {n. n < 2 * M \<and> n mod 2 = 0}"
+      assume "x \<in> snap ` {n. n < 2 * M \<and> n mod 2 = 0}"
       then obtain n where
         n: "n \<in> {n. n < 2 * (card actions)}"
         "n mod 2 = 0"
-        "s n = x" using M_def by auto
+        "snap n = x" using M_def by auto
       hence "n div 2 \<in> {n. n < card actions}" by auto
       hence "act (n div 2) \<in> actions" using act_img_action by blast
-      with n(2, 3)[simplified s_def]
+      with n(2, 3)[simplified snap_def]
       show "x \<in> at_start ` actions" using action_numbering
         by auto
     next
@@ -200,21 +200,21 @@ proof -
         using 1[simplified 2[symmetric]]
         by (subst 3, blast)
       with xa
-      show "x \<in> s ` {n. n < 2 * M \<and> n mod 2 = 0}"
-        unfolding s_def by auto
+      show "x \<in> snap ` {n. n < 2 * M \<and> n mod 2 = 0}"
+        unfolding snap_def by auto
     qed
     ultimately
-    show "bij_betw s {n. n < 2 * M \<and> n mod 2 = 0} (at_start ` actions)"
+    show "bij_betw snap {n. n < 2 * M \<and> n mod 2 = 0} (at_start ` actions)"
       using bij_betw_imageI by blast
   qed
-  have 2: "bij_betw s {n. n < 2 * M \<and> n mod 2 = 1} (at_end ` actions)"
+  have 2: "bij_betw snap {n. n < 2 * M \<and> n mod 2 = 1} (at_end ` actions)"
   proof -
-    have "inj_on s {n. n < 2 * M \<and> n mod 2 = 1}"
+    have "inj_on snap {n. n < 2 * M \<and> n mod 2 = 1}"
     proof (rule inj_onI)
       fix x y
       assume a: "x \<in> {n. n < 2 * M \<and> n mod 2 = 1}" 
              "y \<in> {n. n < 2 * M \<and> n mod 2 = 1}" 
-             "s x = s y" 
+             "snap x = snap y" 
       
       hence 0: "x div 2 \<in> {m. m < M}"
             "y div 2 \<in> {m. m < M}" by auto
@@ -226,7 +226,7 @@ proof -
       
       from a
       have 2: "at_end (act (x div 2)) = at_end (act (y div 2))"  
-        using s_def by auto
+        using snap_def by auto
       
       from 1 2
       have "act (x div 2) = act (y div 2)" 
@@ -240,17 +240,17 @@ proof -
       show "x = y" by (metis div_mod_decomp)
     qed
     moreover
-    have "s ` {n. n < 2 * M \<and> n mod 2 = 1} = (at_end ` actions)"
+    have "snap ` {n. n < 2 * M \<and> n mod 2 = 1} = (at_end ` actions)"
     proof (rule equalityI; rule subsetI)
       fix x
-      assume "x \<in> s ` {n. n < 2 * M \<and> n mod 2 = 1}"
+      assume "x \<in> snap ` {n. n < 2 * M \<and> n mod 2 = 1}"
       then obtain n where
         n: "n \<in> {n. n < 2 * (card actions)}"
         "n mod 2 = 1"
-        "s n = x" using M_def by auto
+        "snap n = x" using M_def by auto
       hence "n div 2 \<in> {n. n < card actions}" by auto
       hence "act (n div 2) \<in> actions" using act_img_action by blast
-      with n(2, 3)[simplified s_def]
+      with n(2, 3)[simplified snap_def]
       show "x \<in> at_end ` actions" using action_numbering
         by auto
     next
@@ -318,11 +318,11 @@ proof -
         using 1[simplified 2[symmetric]]
         by (subst 3, blast)
       with xa
-      show "x \<in> s ` {n. n < 2 * M \<and> n mod 2 = 1}"
-        unfolding s_def by auto
+      show "x \<in> snap ` {n. n < 2 * M \<and> n mod 2 = 1}"
+        unfolding snap_def by auto
     qed
     ultimately
-    show "bij_betw s {n. n < 2 * M \<and> n mod 2 = 1} (at_end ` actions)"
+    show "bij_betw snap {n. n < 2 * M \<and> n mod 2 = 1} (at_end ` actions)"
       using bij_betw_imageI by blast
   qed
   have 3: \<open>{n. n < 2 * M \<and> n mod 2 = 0} \<union> {n. n < 2 * M \<and> n mod 2 = 1} = {n. n < 2 * M}\<close>
@@ -338,35 +338,36 @@ proof -
     ultimately
     show "x \<in> {n. n < 2 * M \<and> n mod 2 = 0} \<union> {n. n < 2 * M \<and> n mod 2 = 1}" by blast
   qed
-  show "bij_betw s {n. n < 2 * M} (at_start ` actions \<union> at_end ` actions)"
+  show "bij_betw snap {n. n < 2 * M} (at_start ` actions \<union> at_end ` actions)"
     using bij_betw_combine[OF 1 2 snaps_disj] 3 by simp
 qed
 
 definition interfering_snaps::"'snap_action \<Rightarrow> 'snap_action list" where
-"interfering_snaps a = sorted_key_list_of_set (inv s) {b. a \<noteq> b \<and> mutex_snap_action a b}"
+"interfering_snaps a = sorted_key_list_of_set (inv snap) {b. a \<noteq> b \<and> mutex_snap_action a b}"
 
 text \<open>This is easier to work with.\<close>
-definition interfering_at_start::"'snap_action \<Rightarrow> 'action list" where
-"interfering_at_start a = sorted_key_list_of_set (inv act) {A. at_start A \<noteq> a \<and> mutex_snap_action a (at_start A)}"
+
+definition interfering_at_start::"'snap_action \<Rightarrow> nat list" where
+"interfering_at_start a = sorted_list_of_set {n. at_start (act n) \<noteq> a \<and> mutex_snap_action a (at_start (act n))}"
 
 definition start_constraints::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint list" where
-"start_constraints a = map (\<lambda>b. GT (Start b) \<epsilon>) (interfering_at_start a)"
+"start_constraints a = map (\<lambda>b. GT (Start (act b)) \<epsilon>) (interfering_at_start a)"
 
-definition interfering_at_end::"'snap_action \<Rightarrow> 'action list" where
-"interfering_at_end a = sorted_key_list_of_set (inv act) {A. at_end A \<noteq> a \<and> mutex_snap_action a (at_end A)}"
+definition interfering_at_end::"'snap_action \<Rightarrow> nat list" where
+"interfering_at_end a = sorted_list_of_set {n. at_end (act n) \<noteq> a \<and> mutex_snap_action a (at_end (act n))}"
 
 definition end_constraints::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint list" where
-"end_constraints a = map (\<lambda>b. GT (End b) \<epsilon>) (interfering_at_end a)"
+"end_constraints a = map (\<lambda>b. GT (End (act b)) \<epsilon>) (interfering_at_end a)"
 
 definition sep::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint" where
 "sep a \<equiv> AND_ALL (start_constraints a @ end_constraints a)"
 
 text \<open>The clock constraints for the precondition\<close>
-definition prop_clocks::"'snap_action \<Rightarrow> ('proposition, 'action) clock list" where
-"prop_clocks a \<equiv> map PropClock (sorted_key_list_of_set (inv p) (pre a))"
+definition pre_clocks::"'snap_action \<Rightarrow> ('proposition, 'action) clock list" where
+"pre_clocks a \<equiv> map PropClock (sorted_key_list_of_set (inv p) (pre a))"
 
 definition pre_constraint::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint" where
-"pre_constraint a \<equiv> AND_ALL (map (\<lambda>c. EQ c 1) (prop_clocks a))"
+"pre_constraint a \<equiv> AND_ALL (map (\<lambda>c. EQ c 1) (pre_clocks a))"
 
 text \<open>The guard constraints\<close>
 definition guard::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint" where
@@ -398,6 +399,60 @@ definition dm_to_exec::"(alpha, ('proposition, 'action) clock, 't, ('proposition
 
 subsubsection \<open>Execution\<close>
 
+definition add_effects::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) clkassn list" where
+"add_effects s \<equiv> map (\<lambda>p. (PropClock p, 1)) (sorted_key_list_of_set (inv p) (adds s))"
+
+definition del_effects::"'snap_action  \<Rightarrow> (('proposition, 'action) clock, 't) clkassn list" where
+"del_effects s \<equiv> map (\<lambda>p. (PropClock p, 1)) (sorted_key_list_of_set (inv p) ((dels s) - (adds s)))"
+
+definition effects::"'snap_action \<Rightarrow> (('proposition, 'action) clock, 't) clkassn list" where
+"effects s \<equiv> del_effects s @ add_effects s"
+
+definition at_start_effects::"'action \<Rightarrow> (('proposition, 'action) clock, 't) clkassn list" where
+"at_start_effects a \<equiv> (Running a, 1) # (ExecStart a, 0) # effects (at_start a)"
+
+definition at_end_effects::"'action \<Rightarrow> (('proposition, 'action) clock, 't) clkassn list" where
+"at_end_effects a \<equiv> (Running a, 0) # (ExecEnd a, 0) # effects (at_start a)"
+
+definition execution::"(alpha, ('proposition, 'action) clock, 't, ('proposition, 'action, 'snap_action) state) transition set" where
+"execution \<equiv> 
+  {(Execution (at_start (act m)), EQ (ExecStart (act m)) 1, Unit, at_start_effects (act m), Execution (at_end (act m))) | m. m < M}
+  \<union> {(Execution (at_start (act m)), true_const, Unit, [], Execution (at_end (act m))) | m. m < M}
+  \<union> {(Execution (at_end (act m)), EQ (ExecEnd (act m)) 1, Unit, at_end_effects (act m), Execution (at_end (act (Suc m)))) | m. Suc m < M}
+  \<union> {(Execution (at_end (act m)), true_const, Unit, [], Decision (at_start (act (Suc m)))) | m. Suc m < M}"
+
+
+subsubsection \<open>Over-all conditions\<close>
+definition over_all_clocks::"'action \<Rightarrow> ('proposition, 'action) clock list" where
+"over_all_clocks a \<equiv> map PropClock (sorted_key_list_of_set (inv p) (over_all a))"
+
+definition action_over_all::"'action \<Rightarrow> (('proposition, 'action) clock, 't) dconstraint" where
+"action_over_all a \<equiv> AND_ALL (map (\<lambda>c. CLE (Running a) c 0) (over_all_clocks a))"
+
+definition over_all_conds::"(('proposition, 'action) clock, 't) dconstraint" where
+"over_all_conds \<equiv> AND_ALL (map (\<lambda>m. action_over_all (act m)) (sorted_list_of_set {m. m < M}))"
+
+definition exec_to_main::"(alpha, ('proposition, 'action) clock, 't, ('proposition, 'action, 'snap_action) state) transition" where
+"exec_to_main \<equiv> (Execution (at_end (act M)), over_all_conds, Unit, [(Delta, 0)], Main)"
+
+subsubsection \<open>The goal\<close>
+definition none_running::"(('proposition, 'action) clock, 't) dconstraint" where
+"none_running \<equiv> AND_ALL (map (\<lambda>m. EQ (Running (act m)) 0) (sorted_list_of_set {m. m < M}))"
+
+definition goal_satisfied::"(('proposition, 'action) clock, 't) dconstraint" where
+"goal_satisfied \<equiv> AND_ALL (map (\<lambda>p. EQ (PropClock p) 1) (sorted_key_list_of_set (inv p) goal))"
+
+definition goal_constraint::"(('proposition, 'action) clock, 't) dconstraint" where
+"goal_constraint \<equiv> AND none_running goal_satisfied"
+
+definition goal_trans::"(alpha, ('proposition, 'action) clock, 't, ('proposition, 'action, 'snap_action) state) transition" where
+"goal_trans \<equiv> (ExecDecoding (act M), goal_constraint, Unit, [], Goal)"
+
+subsubsection \<open>The automaton\<close>
+definition plan_automaton::"(alpha, ('proposition, 'action) clock, 't, ('proposition, 'action, 'snap_action) state) ta" where
+"plan_automaton \<equiv> ({initial_transition} \<union> {main_to_decoding} \<union> prop_decoding 
+  \<union> {prop_decoding_to_exec_decoding} \<union> exec_decoding \<union> {exec_decoding_to_decision_making}
+  \<union> decision_making \<union> {dm_to_exec} \<union> execution \<union> {exec_to_main} \<union> {goal_trans}, invs)"
 end
 
 end
