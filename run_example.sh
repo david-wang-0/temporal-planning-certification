0fe9671f655f4e72e92d6abafd889052302bc550
David Wang <72378768+david-wang-0@users.noreply.github.com>
2025-02-26 13:11:24 +0000

    Add scripts for preliminary testing

---
+++ run_example.sh
@@ -0,0 +1,15 @@
+dir=$1
+domain=$2
+instance=$3
+instance_dir=$(dirname $instance)
+instance_basename=$(basename $instance)
+instance_name="${instance_basename%.*}"
+
+make -C ML ground_compile_cert_check \
+    PDDL_DOMAIN=../$dir/$domain \
+    PDDL_PROBLEM=../$dir/$instance \
+    DOMAIN=../$dir/out/ground_domain_$instance_name.pddl \
+    PROBLEM=../$dir/out/ground_problem_$instance_name.pddl \
+    OUTPUT=../$dir/out/$instance_name.muntax \
+    CERTIFICATE=../$dir/out/$instance_name.cert \
+    CERTIFICATE=../$dir/out/$instance_name.rnm
