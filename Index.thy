e6abd9c8cec8f324605d5323425df19846a510d0
David Wang <72378768+david-wang-0@users.noreply.github.com>
2026-01-20 04:46:17 +0000

    Update Index.thy

--- Index.thy
+++ Index.thy
@@ -13,6 +13,7 @@ text \<open>\<open>\<pi>:\<close>\<close>
 term temp_plan_for_problem_impl
 text \<open>\<open>\<pi>a:\<close>\<close>
 term temp_plan_for_problem_impl'
+
 text \<open>\<open>\<Pi>li:\<close>\<close>
 term temp_planning_problem_list_impl_int
 text \<open>\<open>\<Pi>ali:\<close>\<close>
@@ -21,6 +22,7 @@ text \<open>\<open>\<pi>li:\<close>\<close>
 term temp_plan_for_problem_list_impl_int
 text \<open>\<open>\<pi>ali:\<close>\<close>
 term temp_plan_for_problem_list_impl_int'
+
 text \<open>\<open>\<Pi>liN:\<close>\<close>
 term tp_nta_reduction_model_checking
 text \<open>\<open>\<Pi>aliN:\<close>\<close>
@@ -29,20 +31,20 @@ text \<open>\<open>\<pi>liN:\<close>\<close>
 term tp_nta_reduction_correctness
 text \<open>\<open>\<pi>aliN:\<close>\<close>
 term tp_nta_reduction_correctness'
+
 text \<open>\<open>\<T>:\<close>\<close>
 term Simple_Network_Impl_nat
-(* This is found as 
-Actually Simple_Network_Impl, but the definition cannot be navigated to from here.
-sublocale net_impl: Simple_Network_Impl automata_spec broadcast_spec bounds_spec .
-*)
+text \<open>Or @{locale Simple_Network_Impl}\<close>
+
 text \<open>\<open>\<Pi>Sg:\<close>\<close>
 term ground_ast_problem
-text \<open>\<open>\<Pi>Sg:\<close>\<close>
+text \<open>\<open>\<pi>Sg:\<close>\<close>
 term valid_ground_plan
 text \<open>\<open>\<Pi>SgD:\<close>\<close>
 term ground_ast_problem
+text \<open>Or@{locale ground_ast_problem_defs}\<close>
 text \<open>\<open>\<Pi>S:\<close>\<close>
-term TEMPORAL_PDDL_Semantics.wf_ast_problem
+term wf_ast_problem
 
 find_theorems name: "Simple_Network_Language_Model"
 
